1ac54968b2aba256f531c153b5f689f0500ee4ad
nativity <precious.george@corebiztechnologies.com>
2013-08-02 17:09:41 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,8 @@ Rottenpotatoes::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+  root :to => 'movies#index'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
